f38e2c4928f58ea3fc56a7e4edb81b6fc7188e10
rynftn <85428783+rynftn@users.noreply.github.com>
2025-01-07 20:11:16 +1100

    deleted files

--- src/utils/dates.ts
+++
@@ -1,7 +0,0 @@
-import { stat } from 'node:fs/promises';
-import { join } from 'node:path';
-
-export async function getFileCreationDate(filePath: string): Promise<Date> {
-  const stats = await stat(filePath);
-  return stats.birthtime;
-}
